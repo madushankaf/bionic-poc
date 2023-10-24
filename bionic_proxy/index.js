372e37d8857186a94b691dcf5167abb7fc4043e6
madushankaf <madushankafonseka@live.com>
2023-10-24 12:32:42 +0530

    adding node.js proxy interface

---
+++ bionic_proxy/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const axios = require('axios');
+
+const Campaign = Object.freeze({
+    id: "",
+    createdAt: "",
+    Company: "",
+    CampaginName: "",
+    Advertiser: "",
+  });
+
+const books = {};
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/campaigns", (req, res) => {
+    
+    const id = req.query.id;
+    const url = id ? `http://13.59.213.5:8080/bionic/campaigns?id=${id}` : 'http://13.59.213.5:8080/bionic/campaigns';
+    axios.get(url)
+      .then(response => {
+        res.json(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+});
+
+app.post("/campaigns", (req, res) => {
+  const newCampaign = req.body;
+  axios.post('http://13.59.213.5:8080/bionic/campaigns', newCampaign)
+    .then(response => {
+      res.status(201).json(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+
+app.put("/campaigns", (req, res) => {
+    const updatedCampaign = req.body;
+    const id = updatedCampaign.id;
+    axios.put(`http://13.59.213.5:8080/bionic/campaigns/${id}`, updatedCampaign)
+      .then(response => {
+        res.status(200).json(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+  });
+
+
+
+app.listen(9090, () => {
+  console.log("Server started on port 9090");
+});
